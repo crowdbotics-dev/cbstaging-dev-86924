7ac39d58be087b8a2cfe047152b138cef6459b68
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-31 19:07:44 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tiwari
+admin.site.register(Tiwari)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,23 @@
+from django.conf import settings
 from django.db import models
+class Tiwari(models.Model):
+    'Generated Model'
+    mithi = models.BigIntegerField(blank=True,)
+    one = models.BigIntegerField(null=True,blank=True,)
+    two = models.BinaryField(null=True,blank=True,)
+    four = models.BooleanField(null=True,blank=True,)
+    five = models.CharField(max_length=2562554564653879654687654367896546789645324678964531246574531246587645321456875468746587458765487465864587645879645876458764587645876458764584,null=True,blank=True,)
+    three = models.DateField(auto_now_add=True,null=True,blank=True,)
+    fivee = models.DurationField(null=True,blank=True,)
+    high = models.EmailField(max_length=254,null=True,blank=True,)
+    xvd = models.URLField(null=True,blank=True,)
+    data = models.UUIDField(null=True,blank=True,)
+    fdsfsd = models.TimeField(auto_now=True,null=True,blank=True,)
+    mith = models.TextField(null=True,blank=True,)
+    ning = models.SmallIntegerField(null=True,blank=True,)
+    qwre = models.PositiveSmallIntegerField(null=True,blank=True,)
+    poi = models.SlugField(max_length=50,null=True,blank=True,)
+    mjii = models.PositiveIntegerField(null=True,blank=True,)
+    vvvvvvvfc = models.GenericIPAddressField(protocol="both",unpack_ipv4=True,null=True,blank=True,)
+    fcfc = models.IntegerField(null=True,blank=True,)
+    cfvgvg = models.FloatField(null=True,blank=True,)
